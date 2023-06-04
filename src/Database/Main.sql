10b5ab6f9b490172c9bcabd146d99401343b2348
Nguyen Tuan <lowtee.vn@gmail.com>
2023-06-04 23:47:17 +0700

    first commit

---
+++ src/Database/Main.sql
@@ -0,0 +1,23 @@
+﻿--
+-- Tạo Database
+--
+
+CREATE DATABASE TTCANHAN
+GO
+USE TTCANHAN
+
+--
+-- Cấu trúc bảng THONGTIN
+--
+
+CREATE TABLE THONGTIN (
+	ID varchar(5) primary key,
+	HoLot nvarchar(50) not null,
+	Ten nvarchar(20) not null,
+	NgaySinh date not null,
+	GioiTinh char(1) not null,
+	--- 0 = Not known
+	--- 1 = Male
+	--- 2 = Female
+	--- 9 = Not applicable
+)
